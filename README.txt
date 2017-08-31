7ce8f6ff007edb4585732437530b1de72352599c
pkrishnamachari <30294879+pkrishnamachari@users.noreply.github.com>
2017-08-31 22:21:28 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 This project builds my favorite movie website. To run the program, please execute the following instructions:
 
-Unzip the files in the target folder
-Ensure python version 3.0 and above is installed in the computer
-Ensure the computer is connected to the internet
-Run the entertainment_centre.py either through the command line or a developer environment
+1. Unzip the files in the target folder
+2. Ensure python version 3.0 and above is installed in the computer
+3. Ensure the computer is connected to the internet
+4. Run the entertainment_centre.py either through the command line or a developer environment
 
--- media.py
+++ media.py
@@ -1,16 +1,19 @@
 import webbrowser
 
-class Movie():
-    
-# This class defines the movies and its attributes
 
-    def __init__(self,movie_title, movie_storyline,poster_image,trailer_youtube):
+# This class defines the movies and its attributes
+class Movie():
+    # The constructor method defines the inputs and the outputs for the class
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+# The function opens the youtube trailer for the selected movies
     def show_trailer(self):
-      webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.trailer_youtube_url)
 
-  
+
+
